3869ac4cb889018a108a087c5d76a46910c72cfe
Gannon <161328679+GannonCS@users.noreply.github.com>
2025-06-15 02:57:33 -0700

    Update Edit.jsx

--- Frontend/src/pages/Edit.jsx
+++ Frontend/src/pages/Edit.jsx
@@ -4,6 +4,7 @@ import Navigation from '../components/Navigation';
 import '../App.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import React from 'react';
 
 function Edit({exerciseToEdit}) {
     const navigate = useNavigate();
