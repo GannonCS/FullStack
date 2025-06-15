2bd65d7b203d1898e3841086e1142dbf1b8e2c41
Gannon <161328679+GannonCS@users.noreply.github.com>
2025-06-15 02:38:21 -0700

    Update vite.config.js

--- Frontend/src/vite.config.js
+++ Frontend/src/vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     proxy: {
     '/exercises': {
-      target: 'http://localhost:3000'
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+       secure: false,
       } 
     }
   }
