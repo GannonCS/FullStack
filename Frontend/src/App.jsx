38f0d1d25442e17bd8ceecbeacab6aa38f52e2ea
Gannon <161328679+GannonCS@users.noreply.github.com>
2025-06-15 02:54:36 -0700

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Create from './pages/Create'
